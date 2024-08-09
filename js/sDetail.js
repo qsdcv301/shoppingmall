75ab6934c97c0a3cbba458656118dafc2c0ad3b4
qsdcv301 <qsdcv301@naver.com>
2024-08-09 12:48:49 +0900

    사이즈 관련 디자인 및 js완료

--- js/sDetail.js
+++ js/sDetail.js
@@ -50,3 +50,48 @@ colorRadios.forEach(radio => {
 //     }
 // }); 자작
 
+// function viewdetail(){
+//     const sizeAngle = document.querySelector(".size-box i");
+//     const sizeBoxDetail = document.querySelector(".size-box-detail");
+//     if(sizeAngle.classList.contains("fa-angle-down")){
+//         sizeAngle.classList.remove("fa-angle-down");
+//         sizeAngle.classList.add("fa-angle-up");
+//         sizeBoxDetail.style.display = "block";
+//     }else{
+//         sizeAngle.classList.remove("fa-angle-up");
+//         sizeAngle.classList.add("fa-angle-down");
+//         sizeBoxDetail.style.display = "none";
+//     }
+// 자작 2}
+
+function viewdetail() {
+    const detailBox = document.querySelector(".size-box-detail");
+    const toggleIcon = document.getElementById("toggle-icon");
+    if (detailBox.style.display == "block") {
+        detailBox.style.display = "none";
+        toggleIcon.classList.remove("fa-angle-up");
+        toggleIcon.classList.add("fa-angle-down");
+    } else {
+        detailBox.style.display = "block";
+        toggleIcon.classList.remove("fa-angle-down");
+        toggleIcon.classList.add("fa-angle-up");
+    }
+}
+
+const options = document.querySelectorAll(".option");
+const sizeBox = document.querySelector(".size-box span");
+const toggleIcon = document.getElementById("toggle-icon");
+options.forEach(option => {
+    option.addEventListener("click", (e) => {
+        const svalue = e.target.getAttribute("data-value");
+        console.log(e.target.getAttribute("data-value"));
+        options.forEach(opt => {
+            opt.classList.remove("selected");
+        });
+        e.target.classList.add("selected");
+        sizeBox.textContent = `사이즈 : ${svalue}`;
+        document.getElementById("size").value = svalue;
+        toggleIcon.classList.remove("fa-angle-up");
+        toggleIcon.classList.add("fa-angle-down");
+    });
+});
